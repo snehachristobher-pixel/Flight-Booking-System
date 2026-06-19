951fc09f5d7c840d6d70163da0c3cc78edd7bf93
sneha <snehachristobher@gmail.com>
2026-06-19 13:26:57 +0530

    Fix gmail smtp

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,7 +5,9 @@ const sendEmail = async (email, bookingId, passengerName) => {
     console.log("Sending email to:", email);
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
